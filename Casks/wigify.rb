ef1a027e19ce3786ab5b377a275ad0797293beea
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2026-03-05 18:08:52 +0100

    Update wigify to 0.6.0

--- Casks/wigify.rb
+++ Casks/wigify.rb
@@ -1,8 +1,16 @@
 cask "wigify" do
   version "0.6.0"
-  sha256 "c0d564baeaf556f854f9ac11568f909cecda30b5c555cb9b130b16d7c095d87a"
 
-  url "https://github.com/wigify/wigify/releases/download/v#{version}/Wigify-Mac-#{version}-arm64.zip"
+  on_arm do
+    sha256 "c0d564baeaf556f854f9ac11568f909cecda30b5c555cb9b130b16d7c095d87a"
+    url "https://github.com/wigify/wigify/releases/download/v#{version}/Wigify-Mac-#{version}-arm64.zip"
+  end
+
+  on_intel do
+    sha256 "178d0c3fa47554761e539f71ee9ede4163fb53e97920c04bcea241c6bff95924"
+    url "https://github.com/wigify/wigify/releases/download/v#{version}/Wigify-Mac-#{version}-x64.zip"
+  end
+
   name "Wigify"
   desc "Create and display custom HTML/CSS/JS widgets on your desktop"
   homepage "https://github.com/wigify/wigify"
